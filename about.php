f345d4b42de30743235b12471bd2db6115136274
cync48 <perdana13@mhs.if.its.ac.id>
2015-06-25 22:03:26 +0700

    halaman_about
    ini halaman about

---
+++ about.php
@@ -0,0 +1,200 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Bizz Wow a Corporate Business Category Flat Bootstarp Resposive Website Template | Single :: w3layouts</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,200italic,300,300italic,400italic,600,600italic,700,700italic,900' rel='stylesheet' type='text/css'>
+<!-- js -->
+<script src="js/jquery.min.js"></script>
+<!-- //js -->
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Bizz Wow Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+</head>
+	
+<body>
+	<div class="psd">
+	<div class="container">
+<!-- header -->
+		<div class="header">
+			<div class="logo">
+				<a href="index.html"><img src="images/logo.png" alt=" " /></a>
+			</div>
+			<div class="buy-now" style="float:right;margin-top:3em;margin-left:1em;margin-right:1em;">
+				<a href="single.html" class="hvr-bounce-to-left">LOGIN PESERTA</a>
+			</div>
+			<div class="buy-now" style="float:right;margin-top:3em;margin-left:1em;">
+				<a href="single.html" class="hvr-bounce-to-left">DAFTAR</a>
+			</div>
+			<div class="logo-right">
+				<span class="menu"><img src="images/menu.png" alt=" "/></span>
+				<ul class="nav1">
+					<li><a href="index.html" class="act">HOME</a></li>
+					<li><a href="about.php" class="act">ABOUT US</a></li>
+					<li><a href="#feature" class="act scroll">DESKRIPSI EVENT</a></li>
+					<li><a href="blog.html" class="act">BLOG</a></li>
+					<li><a href="portfolio.html" class="act">PORTFOLIO</a></li>
+					<li><a href="contact.html" class="act">CONTACT US</a></li>
+				</ul>
+			</div>
+			<div class="clearfix"> </div>
+				<!-- script for menu -->
+					<script> 
+						$( "span.menu" ).click(function() {
+						$( "ul.nav1" ).slideToggle( 300, function() {
+						 // Animation complete.
+						});
+						});
+					</script>
+				<!-- //script for menu -->
+		</div>
+<!-- //header -->
+<!-- single -->
+	<div class="single">
+		<div id="sejarah">
+			<img src="images/logo_baru.jpg" alt="logo" style="width:100%;">
+		</div>
+		<div class="col-md-8">
+		</div>
+		<div id="judul" class="col-md-4">
+			<h3 style="font-size:2.5em;">SEJARAH</h3>
+		</div>
+		<div id="isi_sejarah" class="col-md-12">
+			<p>
+				Lorem ipsum dolor sit amet, congue laoreet ut mel, regione disputando vituperatoribus in sea. Ex vix elitr dolore. Has cu nemore ocurreret. At eum dictas lucilius, decore vocibus torquatos in vix. Luptatum facilisi at cum, tollit impetus cum ei, ullum clita pri in. Iuvaret repudiandae no vix.
+
+				Vidisse nominavi ad nec. Omnesque suavitate eam in, ad probo doctus eleifend ius. Id ius nominavi consetetur, ei duo nulla quaerendum, mei no explicari similique. Velit partiendo no mel. Per exerci ceteros ne, indoctum pericula at vel. Ea nam virtute signiferumque, habemus quaerendum vim an.
+
+				Ad elit veniam mediocrem mel. Oratio intellegat sed an, rebum mundi partem sed ut. Mucius voluptua vis id, ex ignota volumus scripserit sit. Ex nulla omnes admodum pri. Per ei mucius oblique fastidii, te populo expetenda nec.
+
+				Ad natum similique reformidans mei. Has in fastidii pertinacia, usu liber convenire omittantur ad. Ne alia erant iusto pri, eum eu ullum perpetua. Eripuit voluptaria liberavisse eam eu. Te dolorem principes quo.
+
+				Te sed vivendo omittantur. Vim ne legimus vocibus senserit, in gloriatur temporibus consectetuer cum. Te quis sensibus argumentum eum, eros omittam eloquentiam no mea. Eos et eirmod dolorem recteque, ei persius apeirian mnesarchum usu. Mel animal utamur eruditi ut. Ea sed numquam tibique denique, has ne quod alii admodum. Eos ne elitr adversarium.
+			</p>
+		</div>
+		<div id="tahun_ini" class="col-md-4">
+			<h3 style="font-size:2.5em;">15th SECOND</h3>
+		</div>
+		<div id="isi_tahun_ini" class="col-md-12">
+			<p>
+				Lorem ipsum dolor sit amet, congue laoreet ut mel, regione disputando vituperatoribus in sea. Ex vix elitr dolore. Has cu nemore ocurreret. At eum dictas lucilius, decore vocibus torquatos in vix. Luptatum facilisi at cum, tollit impetus cum ei, ullum clita pri in. Iuvaret repudiandae no vix.
+
+				Vidisse nominavi ad nec. Omnesque suavitate eam in, ad probo doctus eleifend ius. Id ius nominavi consetetur, ei duo nulla quaerendum, mei no explicari similique. Velit partiendo no mel. Per exerci ceteros ne, indoctum pericula at vel. Ea nam virtute signiferumque, habemus quaerendum vim an.
+
+				Ad elit veniam mediocrem mel. Oratio intellegat sed an, rebum mundi partem sed ut. Mucius voluptua vis id, ex ignota volumus scripserit sit. Ex nulla omnes admodum pri. Per ei mucius oblique fastidii, te populo expetenda nec.
+
+				Ad natum similique reformidans mei. Has in fastidii pertinacia, usu liber convenire omittantur ad. Ne alia erant iusto pri, eum eu ullum perpetua. Eripuit voluptaria liberavisse eam eu. Te dolorem principes quo.
+
+				Te sed vivendo omittantur. Vim ne legimus vocibus senserit, in gloriatur temporibus consectetuer cum. Te quis sensibus argumentum eum, eros omittam eloquentiam no mea. Eos et eirmod dolorem recteque, ei persius apeirian mnesarchum usu. Mel animal utamur eruditi ut. Ea sed numquam tibique denique, has ne quod alii admodum. Eos ne elitr adversarium.
+			</p>
+		</div>
+		<div class="col-md-8"></div>
+		<div class="col-md-8">
+			<div id="isi_dekan" class="buy" style="border-top:1px solid #EFEFEF;
+				border-right:5px solid #10870D;
+				border-bottom:1px solid #EFEFEF;
+				border-left:1px solid #EFEFEF;">
+				<p>
+				Lorem ipsum dolor sit amet, congue laoreet ut mel, regione disputando vituperatoribus in sea. Ex vix elitr dolore. Has cu nemore ocurreret. At eum dictas lucilius, decore vocibus torquatos in vix. Luptatum facilisi at cum, tollit impetus cum ei, ullum clita pri in. Iuvaret repudiandae no vix.
+
+				Vidisse nominavi ad nec. Omnesque suavitate eam in, ad probo doctus eleifend ius. Id ius nominavi consetetur, ei duo nulla quaerendum, mei no explicari similique. Velit partiendo no mel. Per exerci ceteros ne, indoctum pericula at vel. Ea nam virtute signiferumque, habemus quaerendum vim an.
+
+				Ad elit veniam mediocrem mel. Oratio intellegat sed an, rebum mundi partem sed ut. Mucius voluptua vis id, ex ignota volumus scripserit sit. Ex nulla omnes admodum pri. Per ei mucius oblique fastidii, te populo expetenda nec.
+
+				Ad natum similique reformidans mei. Has in fastidii pertinacia, usu liber convenire omittantur ad. Ne alia erant iusto pri, eum eu ullum perpetua. Eripuit voluptaria liberavisse eam eu. Te dolorem principes quo.
+
+				Te sed vivendo omittantur. Vim ne legimus vocibus senserit, in gloriatur temporibus consectetuer cum. Te quis sensibus argumentum eum, eros omittam eloquentiam no mea. Eos et eirmod dolorem recteque, ei persius apeirian mnesarchum usu. Mel animal utamur eruditi ut. Ea sed numquam tibique denique, has ne quod alii admodum. Eos ne elitr adversarium.
+			</p>
+			</div>
+		</div>
+		<div id="foto_dekan" class="col-md-4">
+			<img src="images/dekan.jpg" alt="dekan-feb-ui" style="width:50%;">
+			<h3>Prof. Ari Kuncoro, S.E., M.A., Ph.D</h3>
+			<h4>Dekan Fakultas Ekonomi dan Bisnis Universitas Indonesia</h4>
+		</div>
+		<div id="judul_timeline" class="col-md-12">
+			<h3 style="font-size:2.5em;">TIMELINE ACARA</h3>
+		</div>
+		<div class="col-md-12" style="border-top:2px solid #FFAE00;"></div>
+		<div class="col-md-12">
+			<div id="kalender" class="col-md-3">
+				<h3>14 Februari 2016</h3>
+				<p>Gathering + Technical Meeting</p>
+			</div>
+			<div id="kalender" class="col-md-3">
+				<h3>15 Februari 2016</h3>
+				<p>Seminar<br>Tema = .</p>
+			</div>
+			<div id="kalender" class="col-md-3">
+				<h3>16 Februari 2016</h3>
+				<p>Conference<br>Sub-Tema = <br>1. <br>2. <br>3. <br></p>
+			</div>
+			<div id="kalender" class="col-md-3">
+				<h3>17 Februari 2016</h3>
+				<p>Company Visit + Awarding by .. </p>
+			</div>
+		</div>
+		
+		
+	</div>
+<!-- single -->
+<!-- footer -->
+	<div class="footer">
+		<div class="footer-left">
+			<ul>
+				<li><a href="index.html"><img src="images/logo1.png" alt=" " /></a><span> |</span></li>
+				<li><p>The awesome agency. <span>0800 (123) 4567 // Los Angeles 746 PO</span></p></li>
+			</ul>
+		</div>
+		<div class="footer-right">
+			<p>Template by <a href="http://w3layouts.com/">w3layouts</a></p>
+		</div>
+		<div class="clearfix"> </div>
+	</div>
+<!-- //footer -->
+	</div>
+	</div>
+<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+									$(document).ready(function() {
+										/*
+										var defaults = {
+								  			containerID: 'toTop', // fading element id
+											containerHoverID: 'toTopHover', // fading element hover id
+											scrollSpeed: 1200,
+											easingType: 'linear' 
+								 		};
+										*/
+										
+										$().UItoTop({ easingType: 'easeOutQuart' });
+										
+									});
+	</script>
+<!-- //here ends scrolling icon -->
+</body>
+</html>
---
+++ seminar.php
@@ -0,0 +1,209 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Bizz Wow a Corporate Business Category Flat Bootstarp Resposive Website Template | Single :: w3layouts</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,200italic,300,300italic,400italic,600,600italic,700,700italic,900' rel='stylesheet' type='text/css'>
+<!-- js -->
+<script src="js/jquery.min.js"></script>
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+<!-- //js -->
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Bizz Wow Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+<!-- ---- map ---- -->
+<script>
+    function initialize_nampilin(){
+          var myLatlng = new google.maps.LatLng(-6.360298, 106.826386);
+     
+          var mapOptions = {
+            zoom: 18,
+            center: myLatlng
+          }
+     
+          var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+      
+          var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            //title: 'myLatlng'
+            animation: google.maps.Animation.BOUNCE
+          });
+        }
+        google.maps.event.addDomListener(window, 'load', initialize_nampilin);
+  </script>
+ <!-- ---- map ---- -->
+</head>
+	
+<body>
+	<div class="psd">
+	<div class="container">
+<!-- header -->
+		<div class="header">
+			<div class="logo">
+				<a href="index.html"><img src="images/logo.png" alt=" " /></a>
+			</div>
+			<div class="buy-now" style="float:right;margin-top:3em;margin-left:1em;margin-right:1em;">
+				<a href="single.html" class="hvr-bounce-to-left">LOGIN PESERTA</a>
+			</div>
+			<div class="buy-now" style="float:right;margin-top:3em;margin-left:1em;">
+				<a href="single.html" class="hvr-bounce-to-left">DAFTAR</a>
+			</div>
+			<div class="logo-right">
+				<span class="menu"><img src="images/menu.png" alt=" "/></span>
+				<ul class="nav1">
+					<li><a href="index.html" class="act">HOME</a></li>
+					<li><a href="about.php" class="act">ABOUT US</a></li>
+					<li><a href="#feature" class="act scroll">DESKRIPSI EVENT</a></li>
+					<li><a href="blog.html" class="act">BLOG</a></li>
+					<li><a href="portfolio.html" class="act">PORTFOLIO</a></li>
+					<li><a href="contact.html" class="act">CONTACT US</a></li>
+				</ul>
+			</div>
+			<div class="clearfix"> </div>
+				<!-- script for menu -->
+					<script> 
+						$( "span.menu" ).click(function() {
+						$( "ul.nav1" ).slideToggle( 300, function() {
+						 // Animation complete.
+						});
+						});
+					</script>
+				<!-- //script for menu -->
+		</div>
+<!-- //header -->
+<!-- single -->
+	<div class="single">
+		<div class="col-md-8">
+		</div>
+		<div id="judul" class="col-md-4">
+			<h3 style="font-size:2.5em;">DESKRIPSI</h3>
+		</div>
+		<div id="isi_sejarah" class="col-md-12">
+			<p>
+				Lorem ipsum dolor sit amet, congue laoreet ut mel, regione disputando vituperatoribus in sea. Ex vix elitr dolore. Has cu nemore ocurreret. At eum dictas lucilius, decore vocibus torquatos in vix. Luptatum facilisi at cum, tollit impetus cum ei, ullum clita pri in. Iuvaret repudiandae no vix.
+
+				Vidisse nominavi ad nec. Omnesque suavitate eam in, ad probo doctus eleifend ius. Id ius nominavi consetetur, ei duo nulla quaerendum, mei no explicari similique. Velit partiendo no mel. Per exerci ceteros ne, indoctum pericula at vel. Ea nam virtute signiferumque, habemus quaerendum vim an.
+
+				Ad elit veniam mediocrem mel. Oratio intellegat sed an, rebum mundi partem sed ut. Mucius voluptua vis id, ex ignota volumus scripserit sit. Ex nulla omnes admodum pri. Per ei mucius oblique fastidii, te populo expetenda nec.
+
+				Ad natum similique reformidans mei. Has in fastidii pertinacia, usu liber convenire omittantur ad. Ne alia erant iusto pri, eum eu ullum perpetua. Eripuit voluptaria liberavisse eam eu. Te dolorem principes quo.
+
+				Te sed vivendo omittantur. Vim ne legimus vocibus senserit, in gloriatur temporibus consectetuer cum. Te quis sensibus argumentum eum, eros omittam eloquentiam no mea. Eos et eirmod dolorem recteque, ei persius apeirian mnesarchum usu. Mel animal utamur eruditi ut. Ea sed numquam tibique denique, has ne quod alii admodum. Eos ne elitr adversarium.
+			</p>
+		</div>
+		<div class="col-md-12" style="margin-bottom:8%;">
+			<div id="tema1" class="col-md-6">
+				<div class="col-md-12" style="text-align: center;
+				border-bottom: 3px solid #FFAE00;
+				letter-spacing: -1px;">
+					<h3>Tema 1</h3>
+				</div>
+				<div id="isi_tema1" class="col-md-12">
+					<p>
+						Lorem ipsum dolor sit amet, congue laoreet ut mel, regione disputando vituperatoribus in sea. Ex vix elitr dolore. Has cu nemore ocurreret. At eum dictas lucilius, decore vocibus torquatos in vix. Luptatum facilisi at cum, tollit impetus cum ei, ullum clita pri in. Iuvaret repudiandae no vix.
+
+					Vidisse nominavi ad nec. Omnesque suavitate eam in, ad probo doctus eleifend ius. Id ius nominavi consetetur, ei duo nulla quaerendum, mei no explicari similique. Velit partiendo no mel. Per exerci ceteros ne, indoctum pericula at vel. Ea nam virtute signiferumque, habemus quaerendum vim an.
+
+					Ad elit veniam mediocrem mel. Oratio intellegat sed an, rebum mundi partem sed ut. Mucius voluptua vis id, ex ignota volumus scripserit sit. Ex nulla omnes admodum pri. Per ei mucius oblique fastidii, te populo expetenda nec.
+
+					Ad natum similique reformidans mei. Has in fastidii pertinacia, usu liber convenire omittantur ad. Ne alia erant iusto pri, eum eu ullum perpetua. Eripuit voluptaria liberavisse eam eu. Te dolorem principes quo.
+
+					Te sed vivendo omittantur. Vim ne legimus vocibus senserit, in gloriatur temporibus consectetuer cum. Te quis sensibus argumentum eum, eros omittam eloquentiam no mea. Eos et eirmod dolorem recteque, ei persius apeirian mnesarchum usu. Mel animal utamur eruditi ut. Ea sed numquam tibique denique, has ne quod alii admodum. Eos ne elitr adversarium.
+					</p>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+			<div id="tema1" class="col-md-6">
+				<div class="col-md-12" style="text-align: center;
+				border-bottom: 3px solid #10870D;
+				letter-spacing: -1px;">
+					<h3>Tema 2</h3>
+				</div>
+				<div id="isi_tema2" class="col-md-12">
+					<p>
+						Lorem ipsum dolor sit amet, congue laoreet ut mel, regione disputando vituperatoribus in sea. Ex vix elitr dolore. Has cu nemore ocurreret. At eum dictas lucilius, decore vocibus torquatos in vix. Luptatum facilisi at cum, tollit impetus cum ei, ullum clita pri in. Iuvaret repudiandae no vix.
+
+					Vidisse nominavi ad nec. Omnesque suavitate eam in, ad probo doctus eleifend ius. Id ius nominavi consetetur, ei duo nulla quaerendum, mei no explicari similique. Velit partiendo no mel. Per exerci ceteros ne, indoctum pericula at vel. Ea nam virtute signiferumque, habemus quaerendum vim an.
+
+					Ad elit veniam mediocrem mel. Oratio intellegat sed an, rebum mundi partem sed ut. Mucius voluptua vis id, ex ignota volumus scripserit sit. Ex nulla omnes admodum pri. Per ei mucius oblique fastidii, te populo expetenda nec.
+
+					Ad natum similique reformidans mei. Has in fastidii pertinacia, usu liber convenire omittantur ad. Ne alia erant iusto pri, eum eu ullum perpetua. Eripuit voluptaria liberavisse eam eu. Te dolorem principes quo.
+
+					Te sed vivendo omittantur. Vim ne legimus vocibus senserit, in gloriatur temporibus consectetuer cum. Te quis sensibus argumentum eum, eros omittam eloquentiam no mea. Eos et eirmod dolorem recteque, ei persius apeirian mnesarchum usu. Mel animal utamur eruditi ut. Ea sed numquam tibique denique, has ne quod alii admodum. Eos ne elitr adversarium.
+					</p>
+				</div>
+			</div>
+		</div>	
+		<div id="sejarah">
+			<img src="images/logo_baru.jpg" alt="logo" style="width:100%;">
+		</div>
+		<div id="judul_tempat" class="col-md-4">
+			<h3 style="font-size:2.5em;">LOKASI</h3>
+			<p style="text-align:left;">Gedung Fakultas Ekonomi dan Bisnis Universitas Indonesia</p>
+		</div>
+		<div class="col-md-1"></div>
+		<div id="map" class="col-md-7"></div>
+	</div>
+<!-- single -->
+<!-- footer -->
+	<div class="footer">
+		<div class="footer-left">
+			<ul>
+				<li><a href="index.html"><img src="images/logo1.png" alt=" " /></a><span> |</span></li>
+				<li><p>The awesome agency. <span>0800 (123) 4567 // Los Angeles 746 PO</span></p></li>
+			</ul>
+		</div>
+		<div class="footer-right">
+			<p>Template by <a href="http://w3layouts.com/">w3layouts</a></p>
+		</div>
+		<div class="clearfix"> </div>
+	</div>
+<!-- //footer -->
+	</div>
+	</div>
+<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+									$(document).ready(function() {
+										/*
+										var defaults = {
+								  			containerID: 'toTop', // fading element id
+											containerHoverID: 'toTopHover', // fading element hover id
+											scrollSpeed: 1200,
+											easingType: 'linear' 
+								 		};
+										*/
+										
+										$().UItoTop({ easingType: 'easeOutQuart' });
+										
+									});
+	</script>
+<!-- //here ends scrolling icon -->
+
+</body>
+</html>
